b6ff20b9d17e8608d6627369af812098a787ce0c
SETH OWUSU <your@email.com>
2026-06-24 08:51:45 +0000

    Remove Clerk/Google remnants, restore pre-Clerk login/signup/app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -77,7 +77,6 @@ function MyApp({ Component, pageProps }) {
         <title>{siteConfig.name}</title>
         <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover" />
         <meta name="theme-color" content={siteConfig.themeColor} />
-        <meta name="google-site-verification" content="jpC0jfsDLxjDF1jrW1MUn2ZXB11sMHO5R8lRJB-Vjyw" />
         <link rel="icon" type="image/svg+xml" href={favicon} />
       </Head>
       <ErrorBoundary>
