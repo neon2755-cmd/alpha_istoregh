04c7770864d04a6c5ced9a6b2a2a3abebff0e54f
SETH OWUSU <your@email.com>
2026-06-24 11:20:12 +0000

    fix: add Resend debug logging

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,12 +3,15 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  await resend.emails.send({
+  console.log('Sending email to:', to, '| API Key set:', !!process.env.RESEND_API_KEY);
+  const result = await resend.emails.send({
     from: 'Alpha iStore <onboarding@resend.dev>',
     to,
     subject,
     html,
   });
+  console.log('Resend result:', JSON.stringify(result));
+  return result;
 };
 
 module.exports = sendEmail;
