bcf5312a2986c86aad9759f287f7416de4f8574d
SETH OWUSU <your@email.com>
2026-06-26 01:11:59 +0000

    fix: remove protect() from Clerk middleware

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,11 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher([
-  '/(.*)',
-]);
-
-export default clerkMiddleware((auth, req) => {});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon\\.ico|.*\\..*).*)'],
 };
